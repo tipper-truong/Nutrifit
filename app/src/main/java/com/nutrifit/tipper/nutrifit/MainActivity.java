bbe99b248377a422939f5326838da38a5dd2f356
Tipper Truong <tipper.truong.milo@gmail.com>
2017-11-24 19:38:09 -0800

    Successfully implemented Facebook Login API, next is to store user information to SQLite Database

--- app/src/main/java/com/nutrifit/tipper/nutrifit/MainActivity.java
+++ app/src/main/java/com/nutrifit/tipper/nutrifit/MainActivity.java
@@ -1,9 +1,17 @@
 package com.nutrifit.tipper.nutrifit;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // getKeyHash --> For Facebook Login API
 
         new Handler().postDelayed(new Runnable() {
 
@@ -25,5 +35,25 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         }, 5000);
+    }
+
+    private void getKeyHash()
+    {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.nutrifit.tipper.nutrifit",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- app/src/main/java/com/nutrifit/tipper/nutrifit/SignInActivity.java
+++ app/src/main/java/com/nutrifit/tipper/nutrifit/SignInActivity.java
@@ -1,14 +1,43 @@
 package com.nutrifit.tipper.nutrifit;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
 
 public class SignInActivity extends AppCompatActivity {
 
     private Button newUserSignUpButton;
+    private CallbackManager callbackManager;
+    private ProgressDialog mDialog;
+    private LoginButton fbLoginButton;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +46,15 @@ public class SignInActivity extends AppCompatActivity {
 
         newUserSignUpButton = (Button) findViewById(R.id.newUser_SignUp);
 
-        /** New User? Sign up **/
+        /* New User? Sign up */
+        signUp();
+
+        /* Facebook Login */
+        facebookLogin();
+    }
+
+    private void signUp()
+    {
         newUserSignUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,4 +63,63 @@ public class SignInActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void facebookLogin()
+    {
+        callbackManager = CallbackManager.Factory.create();
+        fbLoginButton = (LoginButton) findViewById(R.id.login_facebook_button);
+        fbLoginButton.setReadPermissions("public_profile", "email");
+        fbLoginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                mDialog = new ProgressDialog(SignInActivity.this);
+                mDialog.setMessage("Retrieving Facebook User Data...");
+                mDialog.show();
+
+                GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        mDialog.dismiss();
+                        Log.d("Response", response.toString());
+                        getFacebookData(object);
+                    }
+                });
+
+                // Request Graph API
+                Bundle parameters = new Bundle();
+                parameters.putString("fields","id, email, first_name, last_name, gender");
+                request.setParameters(parameters);
+                request.executeAsync();
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+        if(AccessToken.getCurrentAccessToken() != null) {
+            Log.v("Email Access Token", AccessToken.getCurrentAccessToken().getUserId());
+        }
+    }
+
+    private void getFacebookData(JSONObject object)
+    {
+        try {
+
+            Log.v("Email", object.getString("email"));
+            Log.v("First Name", object.getString("first_name"));
+            Log.v("Last Name", object.getString("last_name"));
+            Log.v("Gender", object.getString("gender"));
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
